686940ca25d3162684160442ed28be2682e6d972
Darkangel <wtsegars@mac.com>
2019-02-21 19:55:15 -0500

    Assets/

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -6,10 +6,11 @@ var choosenMemeText = document.getElementById('choosen-name');
 var numberOfGuessesLeft = document.getElementById('guesses-left');
 var guessedLetters = document.getElementById('guessed-letters');
 var memeChoice = memeChoices[Math.floor(Math.random() * memeChoices.length)];
-var userGuess = event.key; 
-choosenMemeText.textContent = memeChoice; 
 
+memeChoice.textContent = choosenMemeText;
+numberOfLives.appendChild = numberOfGuessesLeft;
 document.onkeypress = function (event) {
+    var userGuess = event.key; 
     for (let i = 0; i < memeChoices.length; i++) {
         for (let l = 0; l <= memeChoices[i].length; l++) {
             if (userGuess === memeChoices[i][l]) {
@@ -17,11 +18,12 @@ document.onkeypress = function (event) {
             }
             else {
                 numberOfLives--;
+                
             }
 
             if (numberOfLives === 0) {
                 var loserImg = document.createElement('IMG');
-                loserImg.setAttribute('src', '../Images/2u2mf4.jpg');
+                loserImg.setAttribute('src', 'Assets/Images/2u2mf4.jpg');
             }
             if (numberOfLives > 0 && choosenMemeText === memeChoice) {
                 alert('Congraulations! You win!');
@@ -29,27 +31,34 @@ document.onkeypress = function (event) {
                 var winnerImg = document.getElementById('winner-img');
                 document.createElement('IMG');
                 if (i === 'Pepe The Frog') {
-                    winnerImg.setAttribute('src', '../Images/pepefroggie.jpg');
+                    winnerImg.setAttribute('src', 'Assets/Images/pepefroggie.jpg');
                 }
                 else if (i === 'Harambe') {
-                    winnerImg.setAttribute('src', '../Images/d31.jpg');
+                    winnerImg.setAttribute('src', 'Assets/Images/d31.jpg');
                 }
                 else if (i === 'Scumbag Steve') {
-                    winnerImg.setAttribute('src', '../Images/c5b.jpg');
+                    winnerImg.setAttribute('src', 'Assets/Images/c5b.jpg');
                 }
                 else if (i === 'Bad Luck Brian') {
-                    winnerImg.setAttribute('src', '../Images/cab.jpg');
+                    winnerImg.setAttribute('src', 'Assets/Images/cab.jpg');
                 }
                 else if (i === 'Change My Mind') {
-                    winnerImg.setAttribute('src', '../Images/0cb.jpg');
+                    winnerImg.setAttribute('src', 'Assets/Images/0cb.jpg');
                 }
                 else if (i === 'Pedobear') {
-                    winnerImg.setAttribute('src', '../Images/PedoBear.jpg');
+                    winnerImg.setAttribute('src', 'Assets/Images/PedoBear.jpg');
                 }
                 else {
-                    winnerImg.setAttribute('src', '../Images/271.jpg');
+                    winnerImg.setAttribute('src', 'Assets/Images/271.jpg');
                 }
             }
         }
     }
 };
+//var drinkOptions = document.getElementById('drink-options');
+//for (let i = 0; i < drinkList.length; i++) {
+    //var newElm = document.createElement("p");
+    //newElm.textContent = drinkList[i]; 
+   //drinkOptions.appendChild(newElm);
+  //}
+  //
