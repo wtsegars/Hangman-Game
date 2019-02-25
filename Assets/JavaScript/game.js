7fa499a88fdd304e2e7159805b995922f8fbc3d7
Darkangel <wtsegars@mac.com>
2019-02-25 13:10:31 -0500

    Assets/

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -2,58 +2,88 @@ const memeChoices = ['pepe the frog', 'harambe', 'scumbag steve', 'bad luck bria
 
 var numberOfLives = 6;
 
-var choosenMemeText = document.getElementById('choosen-name');
+var choosenMemeText = document.getElementById('choosen-name').textContent;
 var numberOfGuessesLeft = document.getElementById('guesses-left');
 var guessedLetters = document.getElementById('guessed-letters');
-var memeChoice = memeChoices[Math.floor(Math.random() * memeChoices.length)];
+var randomNumber = Math.floor(Math.random() * Math.floor(memeChoices.length));
+var hangman = [];
+var wrong = [];
+var guess = []; 
+var memeChoice = memeChoices[randomNumber];
+console.log(memeChoice);
+for (let i = 0; i < memeChoice.length; i++) {
+    hangman.push('_');
+}
+console.log(hangman);
+// memeChoice = choosenMemeText;
+numberOfGuessesLeft.textContent = numberOfLives;
 
-memeChoice.textContent = choosenMemeText;
-numberOfLives.appendChild = numberOfGuessesLeft;
+// checks guess letter
 document.onkeypress = function (event) {
     var userGuess = event.key; 
+    
     for (let i = 0; i < memeChoice.length; i++) {
-        for (let l = 0; l <= memeChoice[i].length; l++) {
-            if (userGuess === memeChoice[i][l]) {
-                choosenMemeText.textContent = userGuess; 
-            }
-            else {
-                numberOfLives--;
-                
-            }
+        
+        if (userGuess === memeChoice[i]) {
+            const name = document.getElementById('choosen-name');
+            hangman[i] = userGuess; 
+            guess[i] = userGuess
+            name.textContent = hangman.join('');
+            console.log(hangman);
+        }
 
-            if (numberOfLives === 0) {
-                var loserImg = document.createElement('img');
-                loserImg.setAttribute('src', 'Assets/Images/2u2mf4.jpg');
-                if (event.key === 'spacebar') {
-                    location.reload;
-                }
-            }
-            if (numberOfLives > 0 && choosenMemeText === memeChoice) {
-                alert('Congraulations! You win!');
-                alert('Refresh the page to play again.');
-                var winnerImg = document.getElementById('winner-img');
-                document.createElement('IMG');
-                if (i === 'Pepe The Frog') {
-                    winnerImg.setAttribute('src', 'Assets/Images/pepefroggie.jpg');
-                }
-                else if (i === 'Harambe') {
-                    winnerImg.setAttribute('src', 'Assets/Images/d31.jpg');
-                }
-                else if (i === 'Scumbag Steve') {
-                    winnerImg.setAttribute('src', 'Assets/Images/c5b.jpg');
-                }
-                else if (i === 'Bad Luck Brian') {
-                    winnerImg.setAttribute('src', 'Assets/Images/cab.jpg');
-                }
-                else if (i === 'Change My Mind') {
-                    winnerImg.setAttribute('src', 'Assets/Images/0cb.jpg');
-                }
-                else if (i === 'Pedobear') {
-                    winnerImg.setAttribute('src', 'Assets/Images/PedoBear.jpg');
-                }
-                else {
-                    winnerImg.setAttribute('src', 'Assets/Images/271.jpg');
-                }
+        // If statement to determine if the userGuess matches memeChoice[i]
+            // render the char. to the proper div
+        // else
+            // render the char. to the proper div
+            // decrement the lives
+            // render the new lives value to the proper div
+
+        
+    //         if (numberOfLives > 0 && choosenMemeText === memeChoice) {
+    //             alert('Congraulations! You win!');
+    //             alert('Refresh the page to play again.');
+    //             var winnerImg = document.getElementById('winner-img');
+    //             winnerImg.document.createElement('<img>');
+    //             if (i === 'Pepe The Frog') {
+    //                 winnerImg.setAttribute('src', 'Assets/Images/pepefroggie.jpg');
+    //             }
+    //             else if (i === 'Harambe') {
+    //                 winnerImg.setAttribute('src', 'Assets/Images/d31.jpg');
+    //             }
+    //             else if (i === 'Scumbag Steve') {
+    //                 winnerImg.setAttribute('src', 'Assets/Images/c5b.jpg');
+    //             }
+    //             else if (i === 'Bad Luck Brian') {
+    //                 winnerImg.setAttribute('src', 'Assets/Images/cab.jpg');
+    //             }
+    //             else if (i === 'Change My Mind') {
+    //                 winnerImg.setAttribute('src', 'Assets/Images/0cb.jpg');
+    //             }
+    //             else if (i === 'Pedobear') {
+    //                 winnerImg.setAttribute('src', 'Assets/Images/PedoBear.jpg');
+    //             }
+    //             else {
+    //                 winnerImg.setAttribute('src', 'Assets/Images/271.jpg');
+    //             }
+    //         }
+    }
+    guess.push(event.key);
+    guessedLetters.textContent = guess.join(',');
+    const val = memeChoice.indexOf(userGuess)
+    if (val === -1) {
+        numberOfLives--;
+        numberOfGuessesLeft.textContent = numberOfLives;
+        console.log(numberOfLives);
+    }
+    function loseCondition() {
+        if (numberOfLives === 0) {
+            var loserImg = document.getElementById('loser-image');
+            var loserPic = loserImg.attr("src", "Assets/Images/2u2mf4.jpg");
+            loserImg.appendChild(loserPic);
+            document.body.appendChild(loserImg);
+            if (event.key === 'spacebar') {
+               location.reload;
             }
         }
     }
