b6c04313e9ee290c4be8f9975526f8a359d265e0
Darkangel <wtsegars@mac.com>
2019-02-25 19:08:00 -0500

    Assets/

--- Assets/JavaScript/game.js
+++ Assets/JavaScript/game.js
@@ -33,41 +33,6 @@ document.onkeypress = function (event) {
             console.log(totalCorrectCounter);
         }
 
-        // If statement to determine if the userGuess matches memeChoice[i]
-            // render the char. to the proper div
-        // else
-            // render the char. to the proper div
-            // decrement the lives
-            // render the new lives value to the proper div
-
-        
-    //         if (numberOfLives > 0 && choosenMemeText === memeChoice) {
-    //             alert('Congraulations! You win!');
-    //             alert('Refresh the page to play again.');
-    //             var winnerImg = document.getElementById('winner-img');
-    //             winnerImg.document.createElement('<img>');
-    //             if (i === 'Pepe The Frog') {
-    //                 winnerImg.setAttribute('src', 'Assets/Images/pepefroggie.jpg');
-    //             }
-    //             else if (i === 'Harambe') {
-    //                 winnerImg.setAttribute('src', 'Assets/Images/d31.jpg');
-    //             }
-    //             else if (i === 'Scumbag Steve') {
-    //                 winnerImg.setAttribute('src', 'Assets/Images/c5b.jpg');
-    //             }
-    //             else if (i === 'Bad Luck Brian') {
-    //                 winnerImg.setAttribute('src', 'Assets/Images/cab.jpg');
-    //             }
-    //             else if (i === 'Change My Mind') {
-    //                 winnerImg.setAttribute('src', 'Assets/Images/0cb.jpg');
-    //             }
-    //             else if (i === 'Pedobear') {
-    //                 winnerImg.setAttribute('src', 'Assets/Images/PedoBear.jpg');
-    //             }
-    //             else {
-    //                 winnerImg.setAttribute('src', 'Assets/Images/271.jpg');
-    //             }
-    //         }
     }
     guess.push(event.key);
     guessedLetters.textContent = guess.join(',');
@@ -133,10 +98,3 @@ document.onkeypress = function (event) {
             document.body.appendChild(loserImg);
     }
 };
-//var drinkOptions = document.getElementById('drink-options');
-//for (let i = 0; i < drinkList.length; i++) {
-    //var newElm = document.createElement("p");
-    //newElm.textContent = drinkList[i]; 
-   //drinkOptions.appendChild(newElm);
-  //}
-  //
